fe531395400f33bd5e7f8644ee3c3dba1691554a
Huy Nguyen <hi@huynguyen.dev>
2022-05-10 22:38:09 -0700

    First optimization: Scene delegate to prewarm the repository as soon as it has it - By the time Main Feed VC finishes loading its view, the repository should be loading/almost finishing loading the first page! - Can even do this earlier, even in main() but that's overkill for now

--- xkcd/Source/SceneDelegate.swift
+++ xkcd/Source/SceneDelegate.swift
@@ -18,6 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         
         let mainFeedRepository = ReadOnlyComicsRepository()
+        mainFeedRepository.prewarm()
+        
         let mainFeedViewModel = MainFeedViewModel(comicsRepository: mainFeedRepository)
         let mainFeedViewController = MainFeedViewController(viewModel: mainFeedViewModel)
         
