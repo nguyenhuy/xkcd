e3db7694ab44014da010a9e0335a7dc8e6e5f284
Huy Nguyen <hi@huynguyen.dev>
2022-05-14 12:13:24 -0700

    Add test for latestComic()

--- xkcd/xkcdTests/Data/DataSources/RemoteComicDataSourceTests.swift
+++ xkcd/xkcdTests/Data/DataSources/RemoteComicDataSourceTests.swift
@@ -6,47 +6,65 @@
 //
 
 import XCTest
+@testable import xkcd
+import Combine
 
-class URLSessionMock: URLSessionProtocol {
+let ComicJSONFixture = "{\"month\": \"5\", \"num\": ID_TO_BE_REPLACED, \"link\": \"\", \"year\": \"2022\", \"news\": \"\", \"safe_title\": \"Maps\", \"transcript\": \"\", \"alt\": \"OpenStreetMap was always pretty good but is also now *really* good? And Apple Maps's new zoomed-in design in certain cities like NYC and London is just gorgeous. It's cool how there are all these good maps now!\", \"img\": \"https://imgs.xkcd.com/comics/maps.png\", \"title\": \"Maps\", \"day\": \"9\"}"
+
+class NetworkClientMock: NetworkClient {
     var requestedUrls = [URL]()
-    let expectedResponses: [URL : NSString]
+    var returnedPublishers = [URL : AnyPublisher<URLSession.DataTaskPublisher.Output, URLError>]()
     
-    init(expectedResponses: [URL : NSString]) {
-        self.expectedResponses = expectedResponses
+    func dataTaskPublisher(for url: URL) -> AnyPublisher<URLSession.DataTaskPublisher.Output, URLError> {
+        requestedUrls.append(url)
+        guard let publisher = returnedPublishers[url] else {
+            fatalError("Unexpected URL")
+        }
+        return publisher
     }
     
-    override func dataTaskPublisher(for url: URL) -> URLSession.DataTaskPublisher {
-        requestedUrls.append(url)
-        return func dataTaskPublisher(for url: URL) -> URLSession.DataTaskPublisher
+    func mockPublisher(for comicWithId: Int) -> AnyPublisher<URLSession.DataTaskPublisher.Output, URLError> {
+        let JSONString = ComicJSONFixture.replacingOccurrences(of: "ID_TO_BE_REPLACED", with: String(comicWithId))
+        let data = Data(JSONString.utf8)
+        let response = URLResponse()
+        return Result.success((data: data, response: response)).publisher.eraseToAnyPublisher()
     }
 }
 
 class RemoteComicDataSourceTests: XCTestCase {
     let apiHost = "http://test.com"
-    let infoPath = "info.json"
+    let infoPath = "/info.json"
     let decoder = JSONDecoder()
-    var urlSession: URLSession?
-    var dataSource: RemoteComicDataSource?
-
+    var networkClient: NetworkClientMock!
+    var dataSource: RemoteComicDataSource!
+    
     override func setUpWithError() throws {
-        let urlSession = URLSession.shared
-        self.urlSession = urlSession
-        
-        
-        
-        dataSource = RemoteComicDataSource(withUrlSession: urlSession, decoder: decoder, apiHost: apiHost, infoPath: infoPath)
+        networkClient = NetworkClientMock()
+        dataSource = RemoteComicDataSource(networkClient: networkClient, decoder: decoder, apiHost: apiHost, infoPath: infoPath)
     }
+    
+    func testFetchLatestComit() throws {
+        let expectedComicId = 1234
+        let url = URL(string: apiHost + infoPath)!
+        let publisher = networkClient.mockPublisher(for: expectedComicId)
+        networkClient.returnedPublishers[url] = publisher
+        
+        var expectedResult: SingleFetchResult?
+        
+        let expectation = self.expectation(description: "Awaiting result")
+        let cancellable = dataSource.latestComic().sink { _ in
+            expectation.fulfill()
+        } receiveValue: { result in
+            expectedResult = result
+        }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+        waitForExpectations(timeout: 1)
+        cancellable.cancel()
+        
+        let unwrappedResult = try XCTUnwrap(expectedResult)
+        XCTAssertEqual(unwrappedResult.comic.id, expectedComicId)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
+    
+    
 }
