08410fea00b0cc328413e98e8eba1c05bebd5cc5
Huy Nguyen <hi@huynguyen.dev>
2022-05-14 19:22:21 -0700

    Switch app to use SwiftUI and reorganize project structure

---
+++ Source/UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  xkcd
+//
+//  Created by Thanh Huy Nguyen on 5/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Source/xkcdApp.swift
@@ -0,0 +1,17 @@
+//
+//  xkcdApp.swift
+//  xkcd
+//
+//  Created by Thanh Huy Nguyen on 5/14/22.
+//
+
+import SwiftUI
+
+@main
+struct xkcdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
