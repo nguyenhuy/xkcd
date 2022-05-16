912810d47e5db06099e97ac3c3d70661a7235283
Huy Nguyen <hi@huynguyen.dev>
2022-05-15 17:49:31 -0700

    Rename ContentView to MainView

---
+++ Source/UI/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  xkcd
+//
+//  Created by Thanh Huy Nguyen on 5/14/22.
+//
+
+import SwiftUI
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
