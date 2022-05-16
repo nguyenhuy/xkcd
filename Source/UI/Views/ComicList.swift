656be673e62a8ec7792e8444dc47f185fe35cee2
Huy Nguyen <hi@huynguyen.dev>
2022-05-15 20:32:16 -0700

    First implementation of ComicList

--- Source/UI/Views/ComicList.swift
+++ Source/UI/Views/ComicList.swift
@@ -5,4 +5,56 @@
 //  Created by Thanh Huy Nguyen on 5/15/22.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+struct ComicRow: View {
+    let itemState: ComicItemUIState
+    
+    var body: some View {
+        VStack {
+            AsyncImage(url: itemState.imageURL) { image in
+                image.resizable()
+                    .scaledToFit()
+            } placeholder: {
+                Color(uiColor: .lightGray)
+            }
+            .frame(maxWidth: .infinity,
+                   idealHeight: 200)
+            Text("\(itemState.id): \(itemState.title)")
+        }
+    }
+}
+
+struct ComicList<ViewModel>: View where ViewModel: ComicListViewModel {
+    let viewModel: ViewModel
+    
+    var body: some View {
+        List {
+            ForEach(viewModel.uiState.itemStates) { itemState in
+                ComicRow(itemState: itemState)
+            }
+            
+            if viewModel.uiState.hasMore {
+                ProgressView("Loading...")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .onAppear() {
+                        viewModel.fetchNextBatch()
+                    }
+            }
+        }
+        .refreshable {
+            viewModel.refresh()
+        }
+        .navigationTitle(Text("Latest"))
+    }
+}
+
+struct ComicList_Previews: PreviewProvider {
+    static var previews: some View {
+        let repo = ReadOnlyComicRepository()
+        let viewModel = ConcreteComicListViewModel(repository: repo)
+        ComicList(viewModel: viewModel)
+    }
+}
