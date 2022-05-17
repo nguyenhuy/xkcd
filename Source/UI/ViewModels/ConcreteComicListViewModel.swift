69184597fcd7344e378ecdf73a84aa89beaecccc
Huy Nguyen <hi@huynguyen.dev>
2022-05-16 19:35:54 -0700

    Rebuild view model's UI state after bookmarking a comic

--- Source/UI/ViewModels/ConcreteComicListViewModel.swift
+++ Source/UI/ViewModels/ConcreteComicListViewModel.swift
@@ -16,52 +16,10 @@ class ConcreteComicListViewModel: ComicListViewModel {
     init(repository: ComicRepository) {
         self.repository = repository
         self.uiState = ComicListUIState(itemStates: [], errors: [], hasMore: false)
-        self.cancellable = repository.comicsPublisher
-            .map ({ comics in
-                comics.map { comic in
-                    ComicItemUIState(id: comic.id,
-                                     title: comic.title,
-                                     imageURL: comic.imageURL,
-                                     description: comic.alternativeText,
-                                     explainationURL: URL(string: "https://www.explainxkcd.com/wiki/index.php/\(comic.id)")!,
-                                     isBookmarked: false,
-                                     bookmarkAction: {[weak self] in
-                        _ = self?.repository.bookmark(comic: comic)
-                    })
-                }
-            })
-            .map({ states -> [ComicItemUIState] in
-                states.map { state in
-                    var updatedState = state
-                    _ = self.repository.isComicBookmarked(comicId: state.id)
-                        .sink { isBookmarked in
-                            updatedState = ComicItemUIState(id: state.id,
-                                                            title: state.title,
-                                                            imageURL: state.imageURL,
-                                                            description: state.description,
-                                                            explainationURL: state.explainationURL,
-                                                            isBookmarked: isBookmarked,
-                                                            bookmarkAction: state.bookmarkAction)
-                        }
-                    return updatedState
-                }
-            })
-            .map({[weak self] comicItemStates in
-                guard let self = self else { return ComicListUIState(itemStates: [], errors: [], hasMore: false) }
-                
-                let errors = self.uiState.errors
-                let hasMore = self.repository.hasMore()
-                return ComicListUIState(itemStates: comicItemStates,
-                                        errors: errors,
-                                        hasMore: hasMore)
-            })
-            .sink(receiveCompletion: { [weak self] completion in
-                self?.objectWillChange.send()
-            }, receiveValue: { [weak self] newState in
-                self?.uiState = newState
-            })
         
         repository.prewarm()
+        
+        buildUIState(from: repository.comicsPublisher)
     }
     
     func fetchNextBatch() {
@@ -70,5 +28,58 @@ class ConcreteComicListViewModel: ComicListViewModel {
     
     func refresh() {
         repository.refresh()
+    }
+    
+    private func buildUIState(from publisher: Published<[Comic]>.Publisher) {
+        cancellable = publisher.map { comics in
+            comics.map {[weak self] comic in
+                ComicItemUIState(id: comic.id,
+                                 title: comic.title,
+                                 imageURL: comic.imageURL,
+                                 description: comic.alternativeText,
+                                 explainationURL: URL(string: "https://www.explainxkcd.com/wiki/index.php/\(comic.id)")!,
+                                 isBookmarked: false,
+                                 bookmarkAction: self?.bookmarkAction(for: comic) ?? {})
+            }
+        }
+        .map { states -> [ComicItemUIState] in
+            states.map {[weak self] state in
+                var updatedState = state
+                _ = self?.repository.isComicBookmarked(comicId: state.id)
+                    .sink { isBookmarked in
+                        updatedState = ComicItemUIState(id: state.id,
+                                                        title: state.title,
+                                                        imageURL: state.imageURL,
+                                                        description: state.description,
+                                                        explainationURL: state.explainationURL,
+                                                        isBookmarked: isBookmarked,
+                                                        bookmarkAction: state.bookmarkAction)
+                    }
+                return updatedState
+            }
+        }
+        .map { comicItemStates in
+            let errors = self.uiState.errors
+            let hasMore = self.repository.hasMore()
+            return ComicListUIState(itemStates: comicItemStates,
+                                    errors: errors,
+                                    hasMore: hasMore)
+        }
+        .sink(receiveCompletion: { [weak self] completion in
+            self?.objectWillChange.send()
+        }, receiveValue: { [weak self] newState in
+            self?.uiState = newState
+        })
+    }
+    
+    private func bookmarkAction(for comic: Comic) -> (() -> Void){
+        return {[weak self] in
+            _ = self?.repository.bookmark(comic: comic)
+                .sink(receiveCompletion: { _ in }, receiveValue: {[weak self] success in
+                    guard success, let self = self else { return }
+                    // Rebuild UI state
+                    self.buildUIState(from: self.repository.comicsPublisher)
+                })
+        }
     }
 }
